c4f3023151d785b759caf0958170912d9866ce18
belzy <brandon.g.elzy@gmail.com>
2022-03-30 15:11:41 -0500

    Updated test for SocialButtonGroup

---
+++ components/presentation/Buttons/SocialButtonGroup/SocialButtonGroup.spec.tsx
@@ -0,0 +1,14 @@
+import { render, RenderResult, screen} from '@testing-library/react';
+import SocialButton from './SocialButtonGroup';
+import  { TEST_STRINGS } from '../../../../constants/test.constants';
+import '@testing-library/jest-dom';
+
+const { SNAPSHOT_STRINGS } = TEST_STRINGS;
+
+describe('components/SocialButtonGroup', (): void => {
+
+  it('should render', (): void => {
+
+  });
+
+});
