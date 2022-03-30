9af2814719ee776a02e7b7ec1a678f533fc49a1d
belzy <brandon.g.elzy@gmail.com>
2022-03-30 15:04:25 -0500

    Created tests for SocialButton component.

--- components/presentation/Buttons/SocialButton/SocialButton.spec.tsx
+++ components/presentation/Buttons/SocialButton/SocialButton.spec.tsx
@@ -1,35 +1,80 @@
 import { render, RenderResult, screen} from '@testing-library/react';
 import SocialButton from './SocialButton';
 import  { TEST_STRINGS } from '../../../../constants/test.constants';
+import '@testing-library/jest-dom';
 
 const { SNAPSHOT_STRINGS } = TEST_STRINGS;
 
 /**
  * TODO: Write tests
- * * Should render
- * * Should render facebook icon given facebook prop
- * * Should render the correct label given facebook prop.
- * * Should render twitter icon given twitter prop
- * * Should render the correct label given twitter prop.
- * * Should render google icon given google prop
- * * Should render the correct label given google prop.
+ * * [x] Should render
+ * * [x] Should render facebook icon given facebook prop
+ * * [x] Should render the correct label given facebook prop.
+ * * [x] Should render twitter icon given twitter prop
+ * * [x] Should render the correct label given twitter prop.
+ * * [x] Should render google icon given google prop
+ * * [x] Should render the correct label given google prop.
  * * Should handle click event
  * * Snapshot test
  */
 
 let label: string = 'Sign up with ';
-
 describe('components/SocialButton', (): void => {
+  
+  it('should render', (): void => {
 
-  it ('should render', (): void => {
+    render(<SocialButton icon='facebook' />);
 
-    render(<SocialButton />);
+    const socialButtonEl: HTMLElement = screen.getByRole('button');
 
-    const socialButtonEl = screen.queryByRole('button');
-
-    expect(typeof socialButtonEl).toBe(HTMLElement);
+    expect(socialButtonEl).toBeInTheDocument();
   });
 
+  it('should render a Facebook icon given facebook as a prop', (): void => {
 
+    render(<SocialButton icon='facebook' />);
 
+    expect(screen.getByTestId('social-button-icon-facebook')).toBeInTheDocument();
+
+  });
+
+  it('should render the correct label given facebook as a prop', (): void => {
+
+    render(<SocialButton icon='facebook' />);
+
+    expect(screen.getByLabelText(/sign up with facebook/i)).toBeInTheDocument();
+
+  });
+
+  it('should render a Twitter icon given twitter as a prop', (): void => {
+
+    render(<SocialButton icon='twitter' />);
+
+    expect(screen.getByTestId('social-button-icon-twitter')).toBeInTheDocument();
+
+  });
+
+  it('should render the correct label given twitter as a prop', (): void => {
+
+    render(<SocialButton icon='twitter' />);
+
+    expect(screen.getByLabelText(/sign up with twitter/i)).toBeInTheDocument();
+
+  });
+
+  it('should render a Google icon given google as a prop', (): void => {
+
+    render(<SocialButton icon='google' />);
+
+    expect(screen.getByTestId('social-button-icon-google')).toBeInTheDocument();
+
+  });
+
+  it('should render the correct label given google as a prop', (): void => {
+
+    render(<SocialButton icon='google' />);
+
+    expect(screen.getByLabelText(/sign up with google/i)).toBeInTheDocument();
+
+  });
 });
